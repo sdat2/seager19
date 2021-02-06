671435109c829323409f47f378e9b18ba62fb35a
Simon Thomas <sdat2@cam.ac.uk>
2021-02-06 19:49:11 +0000

    backed up data, then started running step 1

---
+++ ocean-model/backup-data.sh
@@ -0,0 +1,7 @@
+# usage: sh ./backup-data.sh 
+name='bk'
+cp -avr RUN/DATA/ RUN/DATA-${name}/
+cp -avr RUN/output/ RUN/output-${name}/
+cp -avr SRC/output/ SRC/output-${name}/
+cp -avr SRC/DATA/ SRC/DATA-${name}/
+cp -avr DATA/ DATA-${name}/
