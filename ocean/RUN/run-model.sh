c577bd1f3ae7552186a8c0f347efdb81c56ebdb8
Simon Thomas <sdat2@cam.ac.uk>
2021-04-05 18:12:33 +0100

    ext: moving model about

--- ocean/RUN/run-model.sh
+++ ocean/RUN/run-model.sh
@@ -2,6 +2,12 @@
 
 # STEP 1 -  spinup the dynamics - ignore the sst equation and use climo windstress
 
+# STEP 1: om_spin, spin.tios
+# STEP 2: om_diag, diag.tios 
+# STEP 3: om_run2f, month.tios 
+
+RUN_NAME="_T1"
+
 echo "A: $(date)" >> timing.txt
 
 ../SRC/tcom.exe -i om_spin -t spin.tios  # 6 minutes
@@ -10,6 +16,8 @@ echo "B: $(date)" >> timing.txt
 
 ../SRC/tios2cdf.exe -f output/om_spin # 3 seconds
 
+cp output/om_spin.nc output/om_spin${RUN_NAME}.nc 
+
 echo "C: $(date)" >> timing.txt
 
 rm -rf output/om_spin.data output/om_spin.indx    # < 1 seconds
@@ -17,13 +25,15 @@ cp -f output/om_spin.save output/om_spin.20y.restart   # < 1 seconds
 
 echo "D: $(date)" >> timing.txt  
 
-# STEP 2 -  diagnose the qflux needed to reproduce sst climatology
+# STEP 2 --  diagnose the qflux needed to reproduce sst climatology
 
 ../SRC/tcom.exe -i om_diag -t diag.tios # 55 seconds
 
 echo "E: $(date)" >> timing.txt  
 
 ../SRC/tios2cdf.exe -f output/om_diag # < 1 seconds
+
+cp output/om_diag.nc output/om_diag${RUN_NAME}.nc 
 
 echo "F: $(date)" >> timing.txt
 
@@ -36,7 +46,7 @@ cp -f output/om_diag.save output/om_diag.2y.restart # < 1 seconds
 # /usr/local/bin/ingrid qflx.ing
 
 
-# STEP 3 -  run the dynamics + sst in full mode
+# STEP 3 -- run the dynamics + sst in full mode
 
 echo "G: $(date)" >> timing.txt
 
@@ -45,6 +55,8 @@ echo "G: $(date)" >> timing.txt
 echo "H: $(date)" >> timing.txt
 
 ../SRC/tios2cdf.exe -f output/om_run2f     # 9 seconds
+
+cp output/om_run2f.nc output/om_run2f${RUN_NAME}.nc 
 
 echo "I: $(date)" >> timing.txt
 
