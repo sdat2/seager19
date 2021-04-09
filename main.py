c9577769ab7261251c89edfb484e81f55d9ccfa5
Simon Thomas <sdat2@cam.ac.uk>
2021-04-09 11:56:38 +0100

    ext: wandb currently accepts the 1.2GB of results files

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ import wandb
 from src.constants import FIGURE_PATH, OCEAN_DATA_PATH, OCEAN_OUTPUT_PATH
 
 
-# 1. Start a W&B run
-wandb.init(
-    project="seager19",
-    entity="sdat2",
-    name="test_4",
-    notes="test run, no files changed",
-)
+def start_wandb() -> None:
+    wandb.init(
+        project="seager19",
+        entity="sdat2",
+        name="test_5",
+        notes="test run, uploading files",
+    )
 
+start_wandb()
 
 # 2. Save model inputs and hyperparameters
 # wandb.config
@@ -27,29 +28,73 @@ wandb.init(
 
 os.system("echo Test!")
 os.system("pwd")
-rc_prefix = "cd ocean/RUN\n"
-os.system("cd ocean/SRC\npwd\nmake all")
 
 
-def run(command: str):
+def compile_all() -> None:
+    os.system("cd ocean/SRC\npwd\nmake all")
+
+compile_all()
+
+
+def run(command: str) -> None:
+    """Runs a line of bash in the ocean/RUN directory
+    and times how long it takes.
+
+    Args:
+        command (str): a valid bash command.
+    """
+    rc_prefix = "cd ocean/RUN\n"
     full_command = rc_prefix + command
     ts = time.perf_counter()
     os.system(full_command)
     te = time.perf_counter()
-    print("{full_command}  %2.5f s\n" % ((te - ts)))
+    print(full_command + " %2.5f s\n" % ((te - ts)))
 
 
-run("../SRC/tcom.exe -i om_spin -t spin.tios")
-run("../SRC/tios2cdf.exe -f output/om_spin")
-run("rm -rf output/om_spin.data output/om_spin.indx")
-run("cp -f output/om_spin.save output/om_spin.20y.restart")
-run("../SRC/tcom.exe -i om_diag -t diag.tios")
-run("../SRC/tios2cdf.exe -f output/om_diag")
-run("rm -rf output/om_diag.data output/om_diag.indx")
-run("cp -f output/om_diag.save output/om_diag.2y.restart")
-run("../SRC/tcom.exe -i om_run2f -t month.tios")
-run("../SRC/tios2cdf.exe -f output/om_run2f ")
-run("rm -rf output/om_run2f.data output/om_run2f.indx")
+def run_all() -> None:
+    """run all the FORTRAN files."""
+    run("../SRC/tcom.exe -i om_spin -t spin.tios")
+    run("../SRC/tios2cdf.exe -f output/om_spin")
+    run("rm -rf output/om_spin.data output/om_spin.indx")
+    run("cp -f output/om_spin.save output/om_spin.20y.restart")
+    run("../SRC/tcom.exe -i om_diag -t diag.tios")
+    run("../SRC/tios2cdf.exe -f output/om_diag")
+    run("rm -rf output/om_diag.data output/om_diag.indx")
+    run("cp -f output/om_diag.save output/om_diag.2y.restart")
+    run("../SRC/tcom.exe -i om_run2f -t month.tios")
+    run("../SRC/tios2cdf.exe -f output/om_run2f")
+    run("rm -rf output/om_run2f.data output/om_run2f.indx")
+
+
+def copy_src_rdir(file_name: str) -> None:
+    run("cp " + file_name + " " + str(wandb.run.dir))
+
+
+def copy_output_rdir(file_name: str) -> None:
+    run("cd output\n cp " + file_name + " " + str(wandb.run.dir))
+
+for x in [
+    "om_diag",
+    "om_diag.tr",
+    "om_diag.log",
+    "om_spin",
+    "om_spin.tr",
+    "om_spin.log",
+    "om_run2f",
+    "om_run2f.tr",
+    "om_run2f.log",
+    "month.tios",
+    "spin.tios",
+    "diag.tios",
+]:
+    copy_src_rdir(x)
+
+for x in [
+    "om_spin.nc",
+    "om_diag.nc",
+    "om_run2f.nc"
+]:
+   copy_output_rdir(x)
 
 # Model training here
 
