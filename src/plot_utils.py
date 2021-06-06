0d892f478fae519af71b19af17d828066a3f113a
Simon Thomas <sdat2@cam.ac.uk>
2021-06-06 19:38:50 +0100

    ext: trying to couple in noteboo

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -188,6 +188,10 @@ def ps_defaults(use_tex: bool = True, dpi: int = 600) -> None:
     Call this at the start of a script which uses `matplotlib`.
     Can enable `matplotlib` LaTeX backend if it is available.
 
+    TODO: There seems to currently be a bug where markers
+         don't appear with 'plt.plot([], [], "x").
+         Need to figure out how I have caused this.
+
     Args:
         use_tex (bool, optional): Whether or not to use latex matplotlib backend.
             Defaults to True.
@@ -223,6 +227,8 @@ def ps_defaults(use_tex: bool = True, dpi: int = 600) -> None:
         "legend.fontsize": 10,
         "xtick.labelsize": 9,
         "ytick.labelsize": 9,
+        # currently I don't use these autofomatters, but hopefully they
+        # are sensible choices to have made.
         "date.autoformatter.year": "%Y",
         "date.autoformatter.month": "%Y-%m",
         "date.autoformatter.day": "%Y-%m-%d",
@@ -236,7 +242,7 @@ def ps_defaults(use_tex: bool = True, dpi: int = 600) -> None:
         # "font.monospace": [],
         "figure.figsize": get_dim(),
         "lines.linewidth": 1.0,
-        "scatter.marker": "+",
+        "scatter.marker": "x",
         "image.cmap": "viridis",
     }
     matplotlib.rcParams.update(p_general)
