9bfdfd0f1be276a6ecf93e4b8379e04a193cadb1
Simon Thomas <sdat2@cam.ac.uk>
2021-06-11 20:35:51 +0100

    fix/ext: fixing unit test and looking at the outputs of the atmos model

--- src/test/test_stress.py
+++ src/test/test_stress.py
@@ -8,18 +8,22 @@ from src.constants import TEST_DIREC
 
 def test_inversibility() -> None:
     """Test inversibility of the stress files using NETCDF3_CLASSIC."""
+
     tau_clim_obj = xr.open_dataarray(
         str(TEST_DIREC / "ocean" / "DATA" / "tau-ECMWF-clim.x"), decode_times=False
     )
     tau_clim_obj.to_netcdf(str(TEST_DIREC / "test.x"), format="NETCDF3_CLASSIC")
+
+    # Only looks at the first 100 bytes - there are some other bits which are
+    # just reordered.
     byte_num = 100
 
     with open(str(TEST_DIREC / "ocean" / "DATA" / "tau-ECMWF-clim.x"), "r+b") as f:
         byt1 = f.read(byte_num)
         print(byt1)
 
-    with open("test.x", "r+b") as f:
+    with open(str(TEST_DIREC / "test.x"), "r+b") as f:
         byt2 = f.read(byte_num)
         print(byt2)
 
-    np.all([byt1[x] == byt2[x] for x in range(len(byt1))])
+    assert np.all([byt1[x] == byt2[x] for x in range(len(byt1))])
