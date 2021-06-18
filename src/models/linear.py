f4623027ea88a3c9758971bf9dd372089ff8a31c
Simon Thomas <sdat2@cam.ac.uk>
2021-06-18 11:23:12 +0100

    ext: investigated model sensitivity

--- src/models/linear.py
+++ src/models/linear.py
@@ -5,6 +5,11 @@ from uncertainties import unumpy as unp
 from scipy.optimize import curve_fit
 
 
+def _parab(x: float, a: float, b: float, c: float):
+    """Fit parabola to data."""
+    return a * (x ** 2) + b * x + c
+
+
 def _lin(x: float, a: float, b: float) -> float:
     """ fit line to data using curve_fit"""
     return (a * x) + b
@@ -24,7 +29,7 @@ def _return_func(popt: np.ndarray, reg_type: str = "lin") -> Callable:
         reg_type (str, optional): Which fit occured. Defaults to "lin".
 
     Returns:
-        Callable: [description]
+        Callable: Function
     """
 
     def lin(x):
@@ -33,10 +38,15 @@ def _return_func(popt: np.ndarray, reg_type: str = "lin") -> Callable:
     def lin_0(x):
         return _lin_0(np.array(x), popt[0])
 
+    def parab(x):
+        return _parab(np.array(x), popt[0], popt[1], popt[2])
+
     if reg_type == "lin":
         return lin
     elif reg_type == "lin0":
         return lin_0
+    elif reg_type == "parab":
+        return parab
     else:
         assert False
 
@@ -56,11 +66,11 @@ def fit(
         Tuple[unp.uarray, Callable]: Paramaters with uncertainty,
             function to put data into.
     """
-    func_dict = {"lin": _lin, "lin0": _lin_0}
+    func_dict = {"lin": _lin, "lin0": _lin_0, "parab": _parab}
 
     assert reg_type in func_dict
 
-    popt, pcov = curve_fit(_lin, x_npa, y_npa)
+    popt, pcov = curve_fit(func_dict[reg_type], x_npa, y_npa)
     perr = np.sqrt(np.diag(pcov))
 
     return unp.uarray(popt, perr), _return_func(popt, reg_type=reg_type)
