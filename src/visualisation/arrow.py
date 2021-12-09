d60689e60338b8f72d1903e5f65e4ed41fc7a5c2
Simon Thomas <sdat2@cam.ac.uk>
2021-12-09 14:40:59 +0000

    ext: reasonable plotting script written

--- src/visualisation/arrow.py
+++ src/visualisation/arrow.py
@@ -1,9 +1,12 @@
 """Arrow plots for mechanism."""
 import os
+from collections import OrderedDict
+from typing import Optional
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
 from src.plot_utils import ps_defaults
 from src.constants import FIGURE_PATH
-from typing import Optional
-import matplotlib.pyplot as plt
 
 
 def plot_arrow_plot(save_path: Optional[str] = None, show_plots: bool = False) -> None:
@@ -228,19 +231,95 @@ def plot_arrow_plot_6(
         plt.clf()
 
 
-if __name__ == "__main__":
-    # python src/visualisation.arrow()
-    plot_arrow_plot_6(save_path=os.path.join(FIGURE_PATH, "mech_arrow_cmip6.pdf"))
-    plot_arrow_plot_6(save_path=os.path.join(FIGURE_PATH, "mech_arrow_cmip6.png"))
-
-
 # Key Format: ${ts}${clt}${sfcwind}${rh}
-RESULTS = {
-    "EEEE": [0.436, 0.377, 0.461, 0.401],
-    "EECE": [0.492, 0.428, 0.522, 0.452],
-    "EEEC": [0.783, 0.646, 0.828, 0.686],
-    "EECC": [0.948, 0.78, 1.002, 0.827],
-    "EE6E": [0.509, 0.436, 0.538, 0.462],
-    "EEE6": [0.601, 0.508, 0.638, 0.543],
-    "EE66": [0.756, 0.627, 0.797, 0.664],
-}
+RESULTS = OrderedDict(
+    [
+        ("EEEE", [0.436, 0.377, 0.461, 0.401]),  # All ECMWF inputs
+        ("EECE", [0.492, 0.428, 0.522, 0.452]),  # cmip5 windspeed
+        ("EEEC", [0.783, 0.646, 0.828, 0.686]),  # cmip5 relative humidity
+        ("EECC", [0.948, 0.78, 1.002, 0.827]),  # cmip5 relative humidity and windspeed
+        ("EE6E", [0.509, 0.436, 0.538, 0.462]),  # cmip6 windspeed
+        ("EEE6", [0.601, 0.508, 0.638, 0.543]),  # cmip6 relative humidity
+        ("EE66", [0.756, 0.627, 0.797, 0.664]),  # cmip6 relative humidity and windspeed
+    ]
+)
+CMIP5_MMM = 0.889
+CMIP6_MMM = 0.772
+ECMWF = 0.411
+
+
+def make_results_xr() -> xr.DataArray:
+    """
+    Make the results xarray from the dictionary above.
+
+    Returns:
+        xr.DataArray: The results xarray object.
+    """
+    key_list = []
+    data_list = []
+    for key in RESULTS:
+        key_list.append(key)
+        data_list.append(RESULTS[key])
+    var_list = ["N", "A", "B", "C"]
+
+    return xr.DataArray(
+        data=np.array(data_list),
+        dims=["mem", "var"],
+        coords=dict(mem=(["mem"], key_list), var=(["var"], var_list)),
+        attrs=dict(
+            description="NINO3.4 trend 1958-2017",
+            units=r"$\Delta$K",
+            long_name="NINO3.4 change 1958-2017",
+        ),
+    ).rename("NINO3.4_TREND")
+
+
+RESULTS_XR = make_results_xr()
+
+
+def plot_results_xr() -> None:
+    """
+    Plot the `RESULTS_XR` object, to make a nice plot for AGU.
+    """
+    delta = 0.1
+    points = [
+        0.5 - 3 * delta / 2,
+        0.5 - delta / 2,
+        0.5 + delta / 2,
+        0.5 + 3 * delta / 2,
+    ]
+    pairs = [("EEEE", "EEEE"), ("EECE", "EE6E"), ("EEEC", "EEE6"), ("EECC", "EE66")]
+    colors = ["blue", "green", "orange", "red"]
+    for pair_number, pair in enumerate(pairs):
+        for i in [0, 1]:
+            for count, letter in enumerate(RESULTS_XR.coords["var"].values):
+                print(str(letter), type(str(letter)))
+                plt.plot(
+                    RESULTS_XR.sel(var=str(letter), mem=pair[i]).values,
+                    1 - i + points[count],
+                    color=colors[pair_number],
+                    marker=r"$" + letter + r"$",
+                )
+
+    plt.plot([ECMWF, ECMWF], [0, 2], colors[0], label="ECMWF/ORAS4")
+    plt.plot([CMIP5_MMM, CMIP5_MMM], [1, 2], "--", color=colors[3], label="CMIP5 MMM")
+    plt.plot([CMIP6_MMM, CMIP6_MMM], [0, 1], ":", color=colors[3], label="CMIP6 MMM")
+    plt.plot([ECMWF, CMIP5_MMM], [2, 2], "--", color="grey")
+    plt.plot([ECMWF, max(CMIP5_MMM, CMIP6_MMM)], [1, 1], "--", color="grey")
+    plt.plot([ECMWF, CMIP5_MMM], [0, 0], "--", color="grey")
+    plt.text(0.35, 1.85, "(a)", color="black")
+    plt.text(0.35, 0.85, "(b)", color="black")
+    plt.xlim([0.3, 1.1])
+    plt.ylim([-0.7, 2.5])
+    plt.yticks([])
+    plt.xlabel(r"NINO3.4 trend 1958-2017 [$\Delta$K]")
+    plt.legend(loc="lower center")
+    plt.savefig("result.png")
+
+
+if __name__ == "__main__":
+    # python src/visualisation/arrow.py
+    print(RESULTS_XR)
+    plot_results_xr()
+    # plot_arrow_plot_6(save_path=os.path.join(FIGURE_PATH, "mech_arrow_cmip6.pdf"))
+    # plot_arrow_plot_6(save_path=os.path.join(FIGURE_PATH, "mech_arrow_cmip6.png"))
